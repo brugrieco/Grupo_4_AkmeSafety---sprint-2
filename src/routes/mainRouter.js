0e5be4a4ce26d3fa684cd8fd04fec893f17155b2
brugrieco <brunonicolasgrieco@gmail.com>
2022-07-25 21:50:24 -0300

    Sprint 3

--- src/routes/mainRouter.js
+++ src/routes/mainRouter.js
@@ -8,11 +8,4 @@ router.get('/', mainController.home);
 
 router.get('/detalle/:id', mainController.productDetail)
 
-router.get('/login', mainController.login)
-
-router.get('/carritoCompras', mainController.carritoCompras)
-
-router.get('/register', mainController.register)
-
-
 module.exports = router;
